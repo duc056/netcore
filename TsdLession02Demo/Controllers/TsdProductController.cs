2de685db93459c7aec5197e0b85593d166cd7dd1
duc056 <tongduc652006@gmail.com>
2026-08-27 14:19:23 +0700

    TsdLession02

---
+++ TsdLession02Demo/Controllers/TsdProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TsdLession02Demo.Models;
+
+namespace TsdLession02Demo.Controllers
+{
+    public class TsdProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.name ="Tống Sỹ Đức";
+            ViewData["Adress"] = "Fit NTU";
+            TempData["UNI"] = "Trường Đại học Nguyễn Trãi";
+
+            return View();
+        }
+        public IActionResult GetProduct()
+        {
+            TsdProduct tsdproduct = new TsdProduct()
+            {
+                ProductId = "P001",
+                ProductName = "Laptop Dell Vostro  ",
+                Price = 22000000,
+                YearRelease = 2024
+            };
+
+            ViewData["ProductVD"] = tsdproduct;
+            ViewBag.ProductVB = tsdproduct;
+
+            return View();
+        }
+    }
+}
---
+++ TsdLession02Demo/Models/TsdProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace TsdLession02Demo.Models
+{
+    public class TsdProduct
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        public int YearRelease { get; set; }
+        public decimal Price { get; set; }
+    }
+}
