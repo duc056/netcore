d983360161682be5ee6a1cbff71544f55dc31e75
duc056 <tongduc652006@gmail.com>
2026-09-09 14:02:59 +0700

    TsdLesion06

---
+++ TsdLession06/TsdLession06/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace TsdLession06.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
