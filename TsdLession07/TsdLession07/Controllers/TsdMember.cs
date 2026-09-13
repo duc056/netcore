545d9ef999c01c18607c7da01f97acadefc5798b
duc056 <tongduc652006@gmail.com>
2026-09-13 16:20:58 +0700

    TsdLession07

---
+++ TsdLession07/TsdLession07/Controllers/TsdMember.cs
@@ -0,0 +1,104 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TsdLession07.Models.DataModels;
+
+namespace TsdLession07.Controllers
+{
+    public class TsdMemberController : Controller
+    {
+        protected static List<TsdMember> _members = new List<TsdMember>
+        {
+            new TsdMember
+            {
+                TsdMemberId = Guid.NewGuid().ToString(),
+                TsdUserName = "TongDuc",
+                TsdPassword = "123456",
+                TsdFullName = "Tống Sỹ Đức",
+                TsdEmail = "tongduc@example.com"
+            },
+            new TsdMember
+            {
+                TsdMemberId = Guid.NewGuid().ToString(),
+                TsdUserName = "tranthibinh",
+                TsdPassword = "123456",
+                TsdFullName = "Trần Thị Bình",
+                TsdEmail = "tranthibinh@example.com"
+            },
+            new TsdMember
+            {
+                TsdMemberId = Guid.NewGuid().ToString(),
+                TsdUserName = "levancuong",
+                TsdPassword = "123456",
+                TsdFullName = "Lê Văn Cường",
+                TsdEmail = "levancuong@example.com"
+            },
+            new TsdMember
+            {
+                TsdMemberId = Guid.NewGuid().ToString(),
+                TsdUserName = "phamthiduyen",
+                TsdPassword = "123456",
+                TsdFullName = "Phạm Thị Duyên",
+                TsdEmail = "phamthiduyen@example.com"
+            },
+            new TsdMember
+            {
+                TsdMemberId = Guid.NewGuid().ToString(),
+                TsdUserName = "hoangminhduc",
+                TsdPassword = "123456",
+                TsdFullName = "Hoàng Minh Đức",
+                TsdEmail = "hoangminhduc@example.com"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_members);
+        }
+
+        public IActionResult GetMember()
+        {
+            var member = new TsdMember
+            {
+                TsdMemberId = Guid.NewGuid().ToString(),
+                TsdUserName = "TongDuc",
+                TsdPassword = "123456",
+                TsdFullName = "Tống Sỹ Đức",
+                TsdEmail = "tongduc@example.com"
+            };
+
+            //ViewBag.Member = member;
+            return View(member);
+        }
+
+        // Đưa dữ liệu dạng List ra View
+        public IActionResult GetMembers()
+        {
+            // Lấy từ mock data
+            ViewBag.Members = _members;
+            return View();
+        }
+
+        // GET: Create member
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Create member
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(TsdMember member)
+        {
+            if (ModelState.IsValid)
+            {
+                member.TsdMemberId = Guid.NewGuid().ToString();
+                _members.Add(member);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(member);
+        }
+
+    }
+}
---
+++ TsdLession07/TsdLession07/Models/DataModels/TsdMember.cs
@@ -0,0 +1,11 @@
+﻿namespace TsdLession07.Models.DataModels
+{
+    public class TsdMember
+    {
+        public string TsdMemberId { get; set; }
+        public string TsdUserName { get; set; }
+        public string TsdPassword { get; set; }
+        public string TsdFullName { get; set; }
+        public string TsdEmail { get; set; }
+    }
+}
